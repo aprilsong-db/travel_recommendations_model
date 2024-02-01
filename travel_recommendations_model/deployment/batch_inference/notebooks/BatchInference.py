9a470498f34e51a8ce99d4b9e93a15bcd961d9fc
April Song <april.song@databricks.com>
2024-02-01 05:39:38 -0800

    fixing some vars

--- travel_recommendations_model/deployment/batch_inference/notebooks/BatchInference.py
+++ travel_recommendations_model/deployment/batch_inference/notebooks/BatchInference.py
@@ -28,7 +28,7 @@ dbutils.widgets.text("input_table_name", "", label="Input Table Name")
 dbutils.widgets.text("output_table_name", "", label="Output Table Name")
 # Unity Catalog registered model name to use for the trained mode.
 dbutils.widgets.text(
-    "model_name", "dev.online.travel_recommendations_model-model", label="Full (Three-Level) Model Name"
+    "model_name", "asong_dev.travel_demo.travel_recommendations_model", label="Full (Three-Level) Model Name"
 )
 
 # COMMAND ----------
